eabd9402a266ad206b05f164de43f9400edc85b3
Hannah Wilcox <hwilcox@nsidc-wilcox.local>
2016-01-24 22:23:12 -0500

    Create javascript to parse access capabilities response and create download link.

--- src/scripts/views/result_item/AccessCapabilitiesView.js
+++ src/scripts/views/result_item/AccessCapabilitiesView.js
@@ -38,8 +38,32 @@ define(['lib/mediator_mixin',
             dataType: 'jsonp'
           })
        .done(function(data) {
-           var content = JSON.stringify(data);
-           console.log("done: " + content);
+           var validOptions = data.validOptions;
+           var defaultOptions = data.defaultOptions;
+
+           _.map(validOptions[0], function(value, key) {
+             var selectClass = key + '-select';
+             element.append('<label>' + key + '</label>');
+             if(_.has(defaultOptions, key)) {
+               element.append('<option selected class="' + selectClass + '" value="' + value + '">' + value + '</option>');
+             } else {
+               element.append('<option class="' + selectClass + '" value="' + value + '">' + value + '</option>');
+             }
+             element.find('.' + selectClass).wrapAll('<select id="' + key + '-select-id"></select>');
+           });
+
+           _.map(defaultOptions, function(value, key){
+             if(!_.has(validOptions[0], key)) {
+               var selectClass = key + '-select';
+               element.append('<label>' + key + '</label>');
+               _.map(value, function(option, label){
+                 element.append('<label>' + label + '</label>');
+                 element.append('<input type="text" class="' + selectClass + '" name="' + label + '" value="' + option + '"></input>');
+               });
+             }
+           });
+
+           element.append("<a href='http://localhost:8081/gi-axe/services/http-get?request=execute&service=WPS&identifier=gi-axe-transform&DataInputs=descriptor%3Dhttp%253A%252F%252Fbcube.geodab.eu%252Fbcube-broker%252Fservices%252Fcswiso%253Frequest%253DGetRecordById%2526version%253D2.0.2%2526service%253DCSW%2526ElementSetName%253Dfull%2526outputSchema%253Dhttp%253A%252F%252Fwww.isotc211.org%252F2005%252Fgmi%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252Fopensearch%25252F1.1%25252Fdataset%25252FGGD906%3BoutputRasterFormat%3Dzip%3BoutputCRS%3DEPSG%253A4326%3BoutputSubset%3D149.6%252C68.63%252C149.6%252C68.63&RawDataOutput=resource'>Download Me!</a>");
            element.html(templates.msgTemplate({messageTitle: 'Returned capabilities', messageContent: content}));
            element.find('#access-capabilities').removeClass('hidden');
           })
