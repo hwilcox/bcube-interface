63fe5695d06215e65aa2d6027167f4ddf7847f16
Hannah Wilcox <hwilcox@vpn-nsidc212-dhcp.int.colorado.edu>
2016-01-25 20:10:27 -0500

    Cleans up code to pass jshint.

--- src/scripts/lib/NsidcOpenSearchResponse.js
+++ src/scripts/lib/NsidcOpenSearchResponse.js
@@ -1,153 +1,153 @@
 define(['lib/utility_functions', 'lib/JSONResults'],
-    function (UtilityFunctions, JSONResults) {
-      function getBoundingBoxFrom(xml) {
-        var bboxes, boxObj, boxArr;
+  function (UtilityFunctions, JSONResults) {
+    function getBoundingBoxFrom(xml) {
+      var bboxes, boxObj, boxArr;
 
-        bboxes = $(xml).filterNode('georss:box'); //$(xml).find('georss\\:box, box');
-        boxArr = [];
+      bboxes = $(xml).filterNode('georss:box'); //$(xml).find('georss\\:box, box');
+      boxArr = [];
 
-        _.each(bboxes, function (box) {
-          var coords = $(box).text().split(' ');
-          boxObj = {
-            north: coords[2],
-            east: coords[3],
-            south: coords[0],
-            west: coords[1]
-          };
+      _.each(bboxes, function (box) {
+        var coords = $(box).text().split(' ');
+        boxObj = {
+          north: coords[2],
+          east: coords[3],
+          south: coords[0],
+          west: coords[1]
+        };
 
-          boxArr.push(boxObj);
-        });
+        boxArr.push(boxObj);
+      });
 
-        return boxArr;
+      return boxArr;
+    }
+
+    function getDateRangeFrom(xml) {
+      var dates, rangeObj, rangeArr, dateRegex = new RegExp('(.*)\/(.*)');
+
+      dates = $(xml).filterNode('dc:date');
+      rangeArr = [];
+
+      _.each(dates, function (date) {
+        var dateParts = $(date).text().match(dateRegex);
+        rangeObj = {
+          startDate: dateParts[1],
+          endDate: dateParts[2]
+        };
+
+        rangeArr.push(rangeObj);
+
+      });
+
+      return rangeArr;
+    }
+
+    function getDataUrlsFrom(xml) {
+      var links, linkArr = [];
+      links = $(xml).find('link[rel=download-data]');
+
+      _.each(links, function (link) {
+        var linkObj = getLinkObj(link);
+        linkArr.push(linkObj);
+      });
+
+      var datasetId = $(xml).filterNode('id').text();
+      var capabilities_link = getBCubeAccessCapabilitesUrls(datasetId);
+      if (!_.isUndefined(capabilities_link)) {
+        var linkObj = {
+          title: 'GI-Tract Data',
+          href: capabilities_link,
+          description: 'Query GI-Cat and return data found.'
+        };
+        linkArr.push(linkObj);
       }
 
-      function getDateRangeFrom(xml) {
-        var dates, rangeObj, rangeArr, dateRegex = new RegExp('(.*)\/(.*)');
+      return linkArr;
+    }
 
-        dates = $(xml).filterNode('dc:date');
-        rangeArr = [];
-
-        _.each(dates, function (date) {
-          var dateParts = $(date).text().match(dateRegex);
-          rangeObj = {
-            startDate: dateParts[1],
-            endDate: dateParts[2]
-          };
-
-          rangeArr.push(rangeObj);
-
-        });
-
-        return rangeArr;
-      }
-
-      function getDataUrlsFrom(xml) {
-        var links, linkArr = [];
-        links = $(xml).find('link[rel=download-data]');
-
-        _.each(links, function (link) {
-          var linkObj = getLinkObj(link);
-          linkArr.push(linkObj);
-        });
-
-        var datasetId = $(xml).filterNode('id').text();
-        var capabilities_link = getBCubeAccessCapabilitesUrls(datasetId);
-        if(!_.isUndefined(capabilities_link)) {
-            var linkObj = {
-                title: 'GI-Tract Data',
-                href: capabilities_link,
-                description: 'Query GI-Cat and return data found.'
-            };
-            linkArr.push(linkObj);
+    function getBCubeAccessCapabilitesUrls(nsidc_id) {
+      var discover_result;
+      var discover_url = 'http://localhost:8081/gi-cat/services/cswisogeo?service=CSW&request=GetRecordById&' +
+        'id=http://nsidc.org/api/opensearch/1.1/dataset/' + nsidc_id +
+        '&outputschema=http://www.isotc211.org/2005/gmi&elementSetName=full';
+      var capabilities_url = 'http://localhost:8081/gi-axe/services/http-get?request=execute&service=WPS&' +
+        'identifier=gi-axe-capabilities&jsondataoutput=true&DataInputs=descriptor%3Dhttp%253A%252F%252F' +
+        'bcube.geodab.eu/bcube-broker%252Fservices%252Fcswiso%253Frequest%253DGetRecordById%2526version%253D' +
+        '2.0.2%2526service%253DCSW%2526ElementSetName%253Dfull%2526outputSchema%253Dhttp%253A%252F%252F' +
+        'www.isotc211.org%252F2005%252Fgmi%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252F' +
+        'opensearch%25252F1.1%25252Fdataset%25252F' + nsidc_id;
+      $.ajax({
+        url: discover_url,
+        dataType: 'xml',
+        async: false,
+        success: function (data) {
+          var ggd906_protocol = String('NSIDCGGD906');
+          $(data).each(function () {
+            var node_text = $(this).text();
+            if (node_text.indexOf(ggd906_protocol) > -1) {
+              discover_result = capabilities_url;
+            }
+          });
         }
+      });
+      return discover_result;
+    }
 
-        return linkArr;
+    function getLinkObj(linkXml) {
+      var linkObj, link = $(linkXml);
+
+      if (link.length > 0) {
+        linkObj = {
+          title: link.attr('title'),
+          href: link.attr('href'),
+          description: link.attr('nsidc:description')
+        };
       }
+      return linkObj;
+    }
 
-      function getBCubeAccessCapabilitesUrls(nsidc_id) {
-          var discover_result = undefined;
-          var discover_url = 'http://localhost:8081/gi-cat/services/cswisogeo?service=CSW&request=GetRecordById&' +
-                    'id=http://nsidc.org/api/opensearch/1.1/dataset/' + nsidc_id +
-                    '&outputschema=http://www.isotc211.org/2005/gmi&elementSetName=full';
-          var capabilities_url = 'http://localhost:8081/gi-axe/services/http-get?request=execute&service=WPS&' +
-              'identifier=gi-axe-capabilities&jsondataoutput=true&DataInputs=descriptor%3Dhttp%253A%252F%252F' +
-              'bcube.geodab.eu/bcube-broker%252Fservices%252Fcswiso%253Frequest%253DGetRecordById%2526version%253D' +
-              '2.0.2%2526service%253DCSW%2526ElementSetName%253Dfull%2526outputSchema%253Dhttp%253A%252F%252F' +
-              'www.isotc211.org%252F2005%252Fgmi%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252F' +
-              'opensearch%25252F1.1%25252Fdataset%25252F' + nsidc_id;
-        $.ajax({
-              url: discover_url,
-              dataType: 'xml',
-              async: false,
-              success: function(data) {
-                var ggd906_protocol = String('NSIDCGGD906');
-                  $(data).each(function () {
-                      var node_text = $(this).text();
-                      if (node_text.indexOf(ggd906_protocol) > -1) {
-                          discover_result = capabilities_url;
-                      }
-                  })
-              }
-            });
-          return discover_result;
+    function getSortedArray(xml, selector) {
+      var arr = UtilityFunctions.getArrayFromjQueryArrayTextContents($(xml).filterNode(selector));
+      if (arr !== undefined) {
+        arr = arr.sort();
       }
+      return _.uniq(arr);
+    }
 
-      function getLinkObj(linkXml) {
-        var linkObj, link = $(linkXml);
+    function processOsEntries(entryXml) {
+      var results = [];
 
-        if (link.length > 0) {
-          linkObj = {
-            title: link.attr('title'),
-            href: link.attr('href'),
-            description: link.attr('nsidc:description')
-          };
-        }
-        return linkObj;
-      }
+      entryXml.find('entry').each(function () {
+        var entryObj, entry = $(this);
 
-      function getSortedArray(xml, selector) {
-        var arr = UtilityFunctions.getArrayFromjQueryArrayTextContents($(xml).filterNode(selector));
-        if (arr !== undefined) {
-          arr = arr.sort();
-        }
-        return _.uniq(arr);
-      }
+        entryObj = {
+          title: entry.filterNode('title').text(),
+          authoritativeId: entry.filterNode('nsidc:authoritativeId').text(),
+          dataUrl: entry.find('link[rel=enclosure]').attr('href'),
+          catalogUrl: entry.find('link[rel=describedBy]').attr('href'),
+          boundingBoxes: getBoundingBoxFrom(this),
+          dateRanges: getDateRangeFrom(this),
+          updated: entry.find('updated').text(),
+          summary: UtilityFunctions.removeWhitespace(UtilityFunctions.removeTags(entry.filterNode('summary').text())),
+          author: UtilityFunctions.getArrayFromjQueryArrayTextContents(entry.filterNode('author')),
+          parameters: getSortedArray(this, 'dif:Detailed_Variable'),
+          keywords: getSortedArray(this, 'dif:Keyword'),
+          dataFormats: getSortedArray(this, 'dif:Distribution'),
+          dataUrls: getDataUrlsFrom(this),
+          orderDataUrl: getLinkObj(entry.find('link[rel=order-data]')),
+          externalDataUrl: getLinkObj(entry.find('link[rel=external-data]')),
+          supportingPrograms: getSortedArray(this, 'nsidc:supportingProgram'),
+          dataCenterNames: getSortedArray(this, 'nsidc:dataCenter')
+        };
+        results.push(entryObj);
+      });
 
-      function processOsEntries(entryXml) {
-        var results = [];
+      return results;
+    }
 
-        entryXml.find('entry').each(function () {
-          var entryObj, entry = $(this);
-
-          entryObj = {
-            title: entry.filterNode('title').text(),
-            authoritativeId: entry.filterNode('nsidc:authoritativeId').text(),
-            dataUrl: entry.find('link[rel=enclosure]').attr('href'),
-            catalogUrl: entry.find('link[rel=describedBy]').attr('href'),
-            boundingBoxes: getBoundingBoxFrom(this),
-            dateRanges: getDateRangeFrom(this),
-            updated: entry.find('updated').text(),
-            summary: UtilityFunctions.removeWhitespace(UtilityFunctions.removeTags(entry.filterNode('summary').text())),
-            author: UtilityFunctions.getArrayFromjQueryArrayTextContents(entry.filterNode('author')),
-            parameters: getSortedArray(this, 'dif:Detailed_Variable'),
-            keywords: getSortedArray(this, 'dif:Keyword'),
-            dataFormats: getSortedArray(this, 'dif:Distribution'),
-            dataUrls: getDataUrlsFrom(this),
-            orderDataUrl: getLinkObj(entry.find('link[rel=order-data]')),
-            externalDataUrl: getLinkObj(entry.find('link[rel=external-data]')),
-            supportingPrograms: getSortedArray(this, 'nsidc:supportingProgram'),
-            dataCenterNames: getSortedArray(this, 'nsidc:dataCenter')
-          };
-          results.push(entryObj);
-        });
-
-        return results;
-      }
-
-      var NsidcOpenSearchResponse = function () {
-        this.fromXml = function (xml, osParameters) {
-          var entryXml = $($.parseXML(xml)),
-              jsonOptions = {
+    var NsidcOpenSearchResponse = function () {
+      this.fromXml = function (xml, osParameters) {
+        var entryXml = $($.parseXML(xml)),
+          jsonOptions = {
             results: processOsEntries(entryXml),
             totalCount: parseInt(entryXml.filterNode('os:totalResults').text(), 10),
             currentIndex: parseInt(entryXml.filterNode('os:startIndex').text(), 10),
@@ -164,10 +164,10 @@ define(['lib/utility_functions', 'lib/JSONResults'],
             facetFilters: osParameters.osFacetFilters
           };
 
-          return new JSONResults(jsonOptions);
-        };
+        return new JSONResults(jsonOptions);
       };
+    };
 
-      return NsidcOpenSearchResponse;
-    }
+    return NsidcOpenSearchResponse;
+  }
 );
--- src/scripts/views/result_item/AccessCapabilitiesView.js
+++ src/scripts/views/result_item/AccessCapabilitiesView.js
@@ -25,6 +25,7 @@ define(['lib/mediator_mixin'
 
       getAccessCapabilities: function (access_capabilities_url) {
         var element = $('.getdata-gi-axe');
+        var this_var = this;
         $.ajax({
             url: access_capabilities_url,
             dataType: 'jsonp',
@@ -35,67 +36,103 @@ define(['lib/mediator_mixin'
             var defaultOptions = data.defaultOptions;
             element.html('');
 
-            _.map(validOptions[0], function (value, key) {
-              var selectClass = key + '-valid';
-              var wrapClass = key + '-wrap';
-              element.append('<label class="' + wrapClass + '">' + key + '</label>');
-              if (_.has(defaultOptions, key)) {
-                element.append('<option selected class="' + selectClass + '" value="' + value + '">' + value + '</option>');
-              } else {
-                element.append('<option class="' + selectClass + '" value="' + value + '">' + value + '</option>');
-              }
-              var selectId = key + '-valid-id';
-              element.find('.' + selectClass).wrapAll('<select class="' + wrapClass + '" id="' + selectId + '"></select>');
-              element.find('.' + wrapClass).wrapAll('<p></p>');
-            });
+            this_var.processValidOptions(validOptions, defaultOptions, element);
 
-            _.map(defaultOptions, function (value, key) {
-              if (!_.has(validOptions[0], key)) {
-                var selectClass = key + '-default';
-                element.append('<label class="' + selectClass + '">' + key + '</label></p>');
-                _.map(value, function (option, label) {
-                  var selectClass = label + '-default';
-                  var selectId = label + '-default-id';
-                  element.append('<label class="' + selectClass + '">' + label + '</label>');
-                  element.append('<input id="' + selectId + '" type="text" class="' + selectClass + '" name="' + label + '" value="' + option + '"></input></p>');
-                  element.find('.' + selectClass).wrapAll('<p></p>');
-                });
-              }
-            });
+            this_var.processDefaultOptions(validOptions, defaultOptions, element);
 
-            var outputCrsValue = _.isUndefined($('#crs-valid-id').val()) ? '' : $('#crs-valid-id').val();
-            var outputRasterFormatValue = _.isUndefined($('#rasterFormat-valid-id').val()) ? '' : $('#rasterFormat-valid-id').val();
-            var outputSpatialSubset = (_.isUndefined($('#west-default-id').val()) ? '' : $('#west-default-id').val()) +
-              (_.isUndefined($('#north-default-id').val()) ? '' : '%252C' + $('#north-default-id').val()) +
-              (_.isUndefined($('#east-default-id').val()) ? '' : '%252C' + $('#east-default-id').val()) +
-              (_.isUndefined($('#south-default-id').val()) ? '' : '%252C' + $('#south-default-id').val());
+            this_var.addBCubeDownloadLink(element);
 
-            element.append("<p><a id='bcube-action' " +
-              "href='http://localhost:8081/gi-axe/services/http-get?" +
-              "request=execute" +
-              "&service=WPS" +
-              "&identifier=gi-axe-transform" +
-              "&DataInputs=descriptor%3Dhttp%253A%252F%252Fbcube.geodab.eu%252Fbcube-broker" +
-              "%252Fservices%252Fcswiso%253Frequest%253DGetRecordById%2526version%253D2.0.2" +
-              "%2526service%253DCSW" +
-              "%2526ElementSetName%253Dfull" +
-              "%2526outputSchema%253Dhttp%253A%252F%252Fwww.isotc211.org%252F2005%252Fgmi" +
-              "%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252Fopensearch%25252F1.1%25252Fdataset%25252FGGD906" +
-              "%3BoutputRasterFormat%3D" + outputRasterFormatValue +
-              "%3BoutputCRS%3D" + outputCrsValue +
-              "%3BoutputSubset%3D" + outputSpatialSubset +
-              "&RawDataOutput=resource'>Download Me!</a>" +
-              "<button id='bcube-action'>Cancel</button></p>");
             $('.get-data').removeClass('open');
           })
           .fail(function (message) {
             element.append('<p>Call to get capabilities failed!</p>');
+            element.append('<p>' + message + '</p>');
             element.removeClass('hidden');
-          })
+          });
+      },
+
+      getWestString: function () {
+        return (_.isUndefined($('#west-default-id').val()) ? '' : $('#west-default-id').val());
+      },
+
+      getNorthString: function () {
+        return (_.isUndefined($('#north-default-id').val()) ? '' : '%252C' + $('#north-default-id').val());
+      },
+
+      getEastString: function () {
+        return (_.isUndefined($('#east-default-id').val()) ? '' : '%252C' + $('#east-default-id').val());
+      },
+
+      getSouthString: function () {
+        return (_.isUndefined($('#south-default-id').val()) ? '' : '%252C' + $('#south-default-id').val());
+      },
+
+      getCrsString: function () {
+        return _.isUndefined($('#crs-valid-id').val()) ? '' : $('#crs-valid-id').val();
+      },
+
+      getRasterFormatString: function () {
+        return _.isUndefined($('#rasterFormat-valid-id').val()) ? '' : $('#rasterFormat-valid-id').val();
+      },
+
+      addBCubeDownloadLink: function (element) {
+        var outputCrsValue = this.getCrsString();
+        var outputRasterFormatValue = this.getRasterFormatString();
+        var outputSpatialSubset = this.getWestString() + this.getNorthString() + this.getEastString() + this.getSouthString();
+
+        element.append('<p><a id=\'bcube-action\' ' +
+          'href=\'http://localhost:8081/gi-axe/services/http-get?' +
+          'request=execute' +
+          '&service=WPS' +
+          '&identifier=gi-axe-transform' +
+          '&DataInputs=descriptor%3Dhttp%253A%252F%252Fbcube.geodab.eu%252Fbcube-broker' +
+          '%252Fservices%252Fcswiso%253Frequest%253DGetRecordById%2526version%253D2.0.2' +
+          '%2526service%253DCSW' +
+          '%2526ElementSetName%253Dfull' +
+          '%2526outputSchema%253Dhttp%253A%252F%252Fwww.isotc211.org%252F2005%252Fgmi' +
+          '%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252Fopensearch%25252F1.1%25252Fdataset%25252FGGD906' +
+          '%3BoutputRasterFormat%3D' + outputRasterFormatValue +
+          '%3BoutputCRS%3D' + outputCrsValue +
+          '%3BoutputSubset%3D' + outputSpatialSubset +
+          '&RawDataOutput=resource\'>Download Me!</a>' +
+          '<button id=\'bcube-action\'>Cancel</button></p>');
+      },
+
+      processValidOptions: function (validOptions, defaultOptions, element) {
+        _.map(validOptions[0], function (value, key) {
+          var selectClass = key + '-valid';
+          var wrapClass = key + '-wrap';
+          element.append('<label class="' + wrapClass + '">' + key + '</label>');
+          if (_.has(defaultOptions, key)) {
+            element.append('<option selected class="' + selectClass + '" value="' + value + '">' + value + '</option>');
+          } else {
+            element.append('<option class="' + selectClass + '" value="' + value + '">' + value + '</option>');
+          }
+          var selectId = key + '-valid-id';
+          element.find('.' + selectClass).wrapAll('<select class="' + wrapClass + '" id="' + selectId + '"></select>');
+          element.find('.' + wrapClass).wrapAll('<p></p>');
+        });
+      },
+
+      processDefaultOptions: function (validOptions, defaultOptions, element) {
+        _.map(defaultOptions, function (value, key) {
+          if (!_.has(validOptions[0], key)) {
+            var selectClass = key + '-default';
+            element.append('<label class="' + selectClass + '">' + key + '</label></p>');
+            _.map(value, function (option, label) {
+              var selectClass = label + '-default';
+              var selectId = label + '-default-id';
+              element.append('<label class="' + selectClass + '">' + label + '</label>');
+              element.append('<input id="' + selectId + '" type="text" class="' + selectClass + '" name="' + label +
+                '" value="' + option + '"/></p>');
+              element.find('.' + selectClass).wrapAll('<p></p>');
+            });
+          }
+        });
       },
 
       hideAccessCapabilities: function () {
-        console.log("hiding element");
+        console.log('hiding element');
         this.$el.addClass('hidden');
         return this;
       },
@@ -111,31 +148,3 @@ define(['lib/mediator_mixin'
     return AccessCapabilitiesView;
   });
 
-'http://localhost:8081/gi-axe/services/http-get?request=execute&' +
-'service=WPS' +
-'&identifier=gi-axe-transform' +
-'&DataInputs=descriptor%3Dhttp%253A%252F%252Fbcube.geodab.eu%252Fbcube-broker%252Fservices%252Fcswiso%253Frequest%253D' +
-'GetRecordById%2526version%253D2.0.2%2526' +
-'service%253DCSW' +
-'%2526ElementSetName%253Dfull' +
-'%2526outputSchema%253Dhttp%253A%252F%252Fwww.isotc211.org%252F2005%252Fgmi' +
-'%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252Fopensearch%25252F1.1%25252Fdataset%25252FGGD906' +
-'%3BoutputRasterFormat%3Dzip' +
-'%3BoutputCRS%3DEPSG:4326' +
-'%3BoutputSubset%3D68.63%252C149.6%252C68.63%252C149.6' +
-'&RawDataOutput=resource'
-
-'http://bcubeaxe.geodab.eu/bcube-broker-axe/services/http-get?' +
-'request=execute&' +
-'service=WPS' +
-'&identifier=gi-axe-transform' +
-'&DataInputs=descriptor%3Dhttp%253A%252F%252Fbcube.geodab.eu%252Fbcube-broker%252Fservices%252Fcswiso%253Frequest' +
-'%253DGetRecordById%2526version%253D2.0.2' +
-'%2526service%253DCSW' +
-'%2526ElementSetName%253Dfull' +
-'%2526outputSchema%253Dhttp%253A%252F%252Fwww.isotc211.org%252F2005%252Fgmi' +
-'%2526id%253Dhttp%25253A%25252F%25252Fnsidc.org%25252Fapi%25252Fopensearch%25252F1.1%25252Fdataset%25252FGGD906' +
-'%3BoutputRasterFormat%3Dzip' +
-'%3BoutputCRS%3DEPSG%253A4326' +
-'%3BoutputSubset%3D149.6%252C68.63%252C149.6%252C68.63' +
-'&RawDataOutput=resource'
