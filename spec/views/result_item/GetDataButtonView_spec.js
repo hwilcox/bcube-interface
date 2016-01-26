6e6e0aac7eb9d7d8d10a5f5ce02c8b35da786685
Hannah Wilcox <hwilcox@vpn-nsidc212-dhcp.int.colorado.edu>
2016-01-25 20:54:27 -0500

    Tries to fix spec.

--- spec/views/result_item/GetDataButtonView_spec.js
+++ spec/views/result_item/GetDataButtonView_spec.js
@@ -1,6 +1,6 @@
-define(['views/result_item/GetDataButtonView', 'views/result_item/AccessCapabilitiesView'],
-  function (GetDataButtonView, AccessCapabilitiesView) {
-    objectFactory.setConfig({
+define(['views/result_item/GetDataButtonView', 'views/result_item/AccessCapabilitiesView', 'lib/ObjectFactory'],
+  function (GetDataButtonView, AccessCapabilitiesView, ObjectFactory) {
+    ObjectFactory.setConfig({
       'AccessCapabilitiesView': AccessCapabilitiesView
     });
 
